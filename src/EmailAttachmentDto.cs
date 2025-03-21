a1c3cf22e9bdd3c54f401ded24f5e6d1ba18738c
Jake Soenneker <jake@soenneker.com>
2025-03-20 19:57:09 -0500

    Initial commit

---
+++ src/EmailAttachmentDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Soenneker.Dtos.Email.Attachment;
+
+/// <summary>
+/// A DTO type for email attachment encapsulation
+/// </summary>
+public class EmailAttachmentDto
+{
+    [JsonPropertyName("fileName")]
+    public string FileName { get; set; } = null!;
+
+    [JsonPropertyName("data")]
+    public byte[] Data { get; set; } = [];
+
+    [JsonPropertyName("mimeType")]
+    public string MimeType { get; set; } = "application/octet-stream";
+}
---
+++ test/Soenneker.Dtos.Email.Attachment.Tests/EmailAttachmentDtoTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Dtos.Email.Attachment.Tests;
+
+[Collection("Collection")]
+public class EmailAttachmentDtoTests : FixturedUnitTest
+{
+    public EmailAttachmentDtoTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
