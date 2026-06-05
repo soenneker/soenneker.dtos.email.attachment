a62938800d5192aee13adf18e9547ae016ae6a4e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:23 -0500

    xml commenting

--- src/Soenneker.Dtos.Email.Attachment/EmailAttachmentDto.cs
+++ src/Soenneker.Dtos.Email.Attachment/EmailAttachmentDto.cs
@@ -7,12 +7,21 @@ namespace Soenneker.Dtos.Email.Attachment;
 /// </summary>
 public class EmailAttachmentDto
 {
+    /// <summary>
+    /// Gets or sets file name.
+    /// </summary>
     [JsonPropertyName("fileName")]
     public string FileName { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets data.
+    /// </summary>
     [JsonPropertyName("data")]
     public byte[] Data { get; set; } = [];
 
+    /// <summary>
+    /// Gets or sets mime type.
+    /// </summary>
     [JsonPropertyName("mimeType")]
     public string MimeType { get; set; } = "application/octet-stream";
 }
